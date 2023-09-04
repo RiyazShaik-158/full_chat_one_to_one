beb7b5ed1a640c3e4e9bfc6beb459e33055685be
Reyaz <riyaz.shaik.1481@gmail.com>
2023-09-04 23:07:20 +0530

    clearingFirebasePersonalDetails

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,16 +4,6 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC_UufKSudOl_zObgvZCLjUe7aWjXnbyUg",
-//   authDomain: "fullchats-5bd2d.firebaseapp.com",
-//   projectId: "fullchats-5bd2d",
-//   storageBucket: "fullchats-5bd2d.appspot.com",
-//   messagingSenderId: "484409470272",
-//   appId: "1:484409470272:web:612e665bc83ab4e7e01ea5"
-// };
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
